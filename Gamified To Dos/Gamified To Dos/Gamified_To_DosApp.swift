ab71fe7efc3c9ba324c0232450ac24ff502dbd2d
Chris Jennison <christopherjennison@gmail.com>
2024-08-16 23:48:28 -0400

    Add files via upload
    Basic implementation of app including the main screen and the daily list.

---
+++ Gamified To Dos/Gamified To Dos/Gamified_To_DosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gamified_To_DosApp.swift
+//  Gamified To Dos
+//
+//  Created by Christopher Jennison on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Gamified_To_DosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
